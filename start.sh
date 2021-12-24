71d052f1eb3655ca5603f0b99b18962d74109457
chennnnnnnnsy <chensylin@gmail.com>
2021-12-24 18:15:48 +0800

    update: dockerfile 基础已完成

--- start.sh
+++ start.sh
@@ -1,13 +1,4 @@
-# 安装 oh-my-zsh 插件 和 移动配置
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-mv /home/chensy/configureFiles/config-zshrc /home/chensy/.zshrc
-
 # 安装 Rust 和 cargo换源
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-mv /home/chensy/configureFiles/config-cargo /home/chensy/.cargo/config
-
-# 切换shell
-chsh -s /bin/zsh
-
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh \
+	&& cp /home/chensy/configureFiles/config-cargo /home/chensy/.cargo/config
 
