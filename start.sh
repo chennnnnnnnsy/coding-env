77df0635e56d27517046074137200da1d4210ebe
chennnnnnnnsy <chensylin@gmail.com>
2021-12-26 23:28:19 +0800

    update: 优化start.sh

--- start.sh
+++ start.sh
@@ -1,37 +1,14 @@
 myhome="/home/chensy"
 
 ssh-keygen -C "code.chensy.lin@outlook.com"
-
-function installLangs () {
-	langs=(\
-		'nodejs-lts-gallium' \
-		'yarn' \
-		'lua' \
-		'rustup' \
-	)
-
-	installCMD='sudo pacman -S --noconfirm'
-
-	for i in ${langs[*]}
-	do
-		installCMD=$installCMD' '${i}
-	done
-
-	${installCMD} && rustup toolchain install stable
-}
-
-function moveConfigureFiles () {
-	mkdir $HOME/.cargo && mv $HOME/configureFiles/config-cargo  $HOME/.cargo/config
-}
-
-installLangs
-
-moveConfigureFiles
+mkdir $HOME/.cargo && mv $HOME/configureFiles/config-cargo  $HOME/.cargo/config
 
 node -v
 yarn -v
 cargo --version
 lua -v
+python --version
+pip -V
 
 echo "----------- Done ------------"
 
