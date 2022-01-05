6fa292a350c2f4b2942011319179052e09067abc
chennnnnnnnsy <chensylin@gmail.com>
2022-01-05 19:21:00 +0800

    fix: remove useless code

--- start.sh
+++ start.sh
@@ -1,23 +1,14 @@
 echo "---------- 开始初始化 ----------"
 echo -e "\n"
 
-sudo chown -R  chensy:lin $HOME/extra
+# sudo chown -R  chensy:lin $HOME/extra
 
 ssh-keygen -C "code.chensy.lin@outlook.com"
 
 echo "ssh 钥匙生成！"
 echo -e "\n"
 
-echo "=============="
-echo "===软件版本==="
-echo "=============="
-
-echo "node" `node -v`
-echo "yarn" `yarn -v`
-cargo --version
-lua -v
-python --version
-pip -V
+yarn global add pyright bash-language-server @tailwindcss/language-server svelte-language-server vls typescript typescript-language-server vscode-langservers-extracted prettier
 
 echo -e  "\n"
 echo "----------- 初始化完成！------------"
