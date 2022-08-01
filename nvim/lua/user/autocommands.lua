7b0ad5d5f0224e69785b1d857a762079ace397c2
ex_lincx3 <ex_lincx3@partner.midea.com>
2022-08-01 09:55:25 +0800

    feat: 去掉vim-illuminate自动命令

--- nvim/lua/user/autocommands.lua
+++ nvim/lua/user/autocommands.lua
@@ -1,11 +1,3 @@
--- illuminate_augroup
-local ill_id = vim.api.nvim_create_augroup('illuminate_augroup', {clear = false})
-vim.api.nvim_create_autocmd("VimEnter", {
-  pattern = "*",
-  group = ill_id,
-  command = "hi illuminatedWord cterm=undercurl gui=undercurl"
-})
-
 -- packer_user_config
 local packer_id = vim.api.nvim_create_augroup('packer_user_config', {clear = false})
 vim.api.nvim_create_autocmd("BufWritePost", {
@@ -13,3 +5,4 @@ vim.api.nvim_create_autocmd("BufWritePost", {
   group = packer_id,
   command = "source <afile> | PackerSync"
 })
+
