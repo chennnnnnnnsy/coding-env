736b01ee7d893c0011b2ade8ebf581eb5c735d5c
chensy <chensy@chensydeMacBook-Pro.local>
2022-08-02 13:20:19 +0800

    feat: update nvim-tree config

--- nvim/mac_install.sh
+++ nvim/mac_install.sh
@@ -10,9 +10,8 @@ yarn global add \
         prettier
         pyright \
 
-brew update && brew install llvm  lua-language-server luarocks cmake autopep8
+# nvim的项目中全局搜 ripgrep
+brew update && brew install llvm  lua-language-server luarocks cmake autopep8 ripgrep
 
-# 主要用在nvim的项目中全局搜索
-brew install ripgrep
 
 luarocks install --server=https://luarocks.org/dev luaformatter
