4ba107471fd1aa77a3d78c5e77bd6383081eaa64
chensy <ex_lincx4@partner.midea.com>
2026-05-20 17:52:22 +0800

    feat(nvim): 初始化

---
+++ nvim/init.lua
@@ -0,0 +1,5 @@
+rocks = {}
+
+require("config.options")
+require("config.keymaps")
+require("config.lazy")
---
+++ nvim/lua/config/keymaps.lua
@@ -0,0 +1,14 @@
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+
+vim.keymap.set("n", "s", "<Nop>", { desc= "不设置s键", silent = true})
+vim.keymap.set("n", "<C-q>", "<cmd>q<CR>", { desc= "退出", silent = true})
+vim.keymap.set("n", "<C-s>", "<cmd>w<CR>", { desc= "保存", silent = true})
+vim.keymap.set("n", "H", "^", { desc= "到行首", silent = true})
+vim.keymap.set("n", "L", "$", { desc= "到行尾", silent = true})
+vim.keymap.set("n", "P", "\"+p", { desc= "系统粘贴", silent = true})
+vim.keymap.set("n", "Y", "\"+y", { desc= "系统复制", silent = true})
+vim.keymap.set("v", "Y", "\"+y", { desc= "系统复制", silent = true})
+vim.keymap.set("n", "<leader>n", "<Cmd>nohl<CR>", { desc= "取消高亮", silent = true})
+vim.keymap.set("n", "n", "nzz", { silent = true})
+vim.keymap.set("n", "N", "Nzz", { silent = true})
